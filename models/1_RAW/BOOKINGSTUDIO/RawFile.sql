ace9a2f40c53e2a0e29a99afd5b59309b8749749
aparnakadlag021992@gmail.com <aparnakadlag021992@gmail.com>
2022-08-25 07:23:49 +0000

    testcommit

---
+++ models/1_RAW/BOOKINGSTUDIO/RawFile.sql
@@ -0,0 +1,5 @@
+with rawone as(
+    select * from ANALYTICS.DBT_AKADLAG.STG_CUSTOMERS
+)
+select * from rawone
+
---
+++ models/2_STANDARD/BOOKINGSTUDIO/stanadard.sql
@@ -0,0 +1,3 @@
+with stanadrd as(
+    select * from ANALYTICS.DBT_AKADLAG.STG_CUSTOMERS
+)select * from stanadrd
